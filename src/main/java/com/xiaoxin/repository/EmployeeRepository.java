ffb7752c293fa2c57070aefed2620244e4b976e9
xiaoxinLiao <13710399496@163.com>
2018-02-10 17:11:12 +0800

    spring data @Query 的使用

--- src/main/java/com/xiaoxin/repository/EmployeeRepository.java
+++ src/main/java/com/xiaoxin/repository/EmployeeRepository.java
@@ -1,19 +1,23 @@
 package com.xiaoxin.repository;
 
 import com.xiaoxin.domain.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 /**
  * Repository<T,ID> : T:Target目标类 ID: 主键类型
+ *
  * @author xiaoxin Liao
  * @date 2018/2/10
  */
-@RepositoryDefinition(domainClass = Employee.class,idClass = Integer.class)
+@RepositoryDefinition(domainClass = Employee.class, idClass = Integer.class)
 public interface EmployeeRepository {
     /**
      * 根据名称查询
+     *
      * @param name 要查询的名字
      * @return 返回查询到的Employee对象
      */
@@ -21,24 +25,85 @@ public interface EmployeeRepository {
 
     /**
      * 查询所有Employee
+     *
      * @return 返回所有Employee 姓名不为空
      */
     List<Employee> findAllByNameIsNotNullOrderByAge();
 
     /**
-     *  where name like ?% and age < ?
+     * where name like ?% and age < ?
+     *
      * @param startWith 名字以startWith 开头
-     * @param lessThan 年龄小于 lessThan
+     * @param lessThan  年龄小于 lessThan
      * @return 满足查询条件的结果
      */
     List<Employee> findByNameStartingWithAndAgeLessThan(String startWith, Integer lessThan);
 
     /**
-     *  where name in(?,?...) or age < ?
-     *  查询在指定姓名集或者年龄不小于指定值的{@link Employee}
+     * where name in(?,?...) or age < ?
+     * 查询在指定姓名集或者年龄不小于指定值的{@link Employee}
+     *
      * @param names 指定的姓名集合
-     * @param age 指定的年龄
+     * @param age   指定的年龄
      * @return 满足查询条件的结果
      */
-    List<Employee> findByNameInOrAgeLessThan(List<String> names,Integer age);
+    List<Employee> findByNameInOrAgeLessThan(List<String> names, Integer age);
+
+    /**
+     * 查询ID 最大的 {@link Employee}
+     *
+     * @return 返回id最大的Employee
+     */
+    @Query("select e from Employee e where id=(select max(id) from Employee e1)")
+    Employee getEmployeeByMaxId();
+
+    /**
+     * 带占位符的索引参数查询
+     *
+     * @param name 第1个索引参数，name
+     * @param age  第2个索引参数，age
+     * @return 满足条件的结果
+     */
+    @Query("select o from Employee o where o.name=?1 and o.age=?2")
+    List<Employee> queryParams1(String name, Integer age);
+
+
+    /**
+     * 命名参数的查询
+     * o.name=:name , @Param("name") String name
+     * :后面的参数名要和@Param("name")的value对应
+     *
+     * @param name 第1个命名参数，name
+     * @param age  第2个命名参数，age
+     * @return 满足条件的结果
+     */
+    @Query("select o from Employee o where o.name=:name and o.age=:age")
+    List<Employee> queryParams2(@Param("name") String name, @Param("age") Integer age);
+
+    /**
+     * 索引参数的模糊查询
+     *
+     * @param name 匹配的姓名
+     * @return 匹配到的 Employee
+     */
+    @Query(value = "select o from Employee o where o.name like %?1%")
+    List<Employee> queryLike1(String name);
+
+    /**
+     * 命名参数的模糊查询
+     *
+     * @param name 匹配的姓名
+     * @return 匹配到的 Employee
+     */
+    @Query("select o from Employee o where o.name like %:name%")
+    List<Employee> queryLike2(@Param("name") String name);
+
+    /**
+     * 原生查询方法，使用表查询 将nativeQuery属性置为True
+     * @return 条数
+     */
+    @Query(nativeQuery = true, value = "SELECT count(1) from employee")
+    long getCount();
+
 }
+
--- src/test/java/com/xiaoxin/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/xiaoxin/repository/EmployeeRepositoryTest.java
@@ -19,26 +19,80 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-data.xml")
 public class EmployeeRepositoryTest {
+
+    @Test
+    public void getCount() throws Exception {
+        long count = repository.getCount();
+        System.out.println("count=" + count);
+        Assert.assertTrue(count > 0);
+    }
+
+    @Test
+    public void queryLike2() throws Exception {
+        List<Employee> employeeList = repository.queryLike2("test1");
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+        Assert.assertTrue(employeeList.size() > 0);
+
+    }
+
+    @Test
+    public void queryLike1() throws Exception {
+        List<Employee> employeeList = repository.queryLike1("test");
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+        Assert.assertTrue(employeeList.size() > 0);
+    }
+
+    @Test
+    public void queryParams2() throws Exception {
+        List<Employee> employeeList = repository.queryParams2("LUBAN", 18);
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+        Assert.assertTrue(employeeList.size() > 0);
+
+    }
+
+    @Test
+    public void queryParams1() throws Exception {
+        List<Employee> employeeList = repository.queryParams1("test2", 20);
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+        Assert.assertTrue(employeeList.size() > 0);
+    }
+
+    @Test
+    public void getEmployeeByMaxId() throws Exception {
+        Employee employee = repository.getEmployeeByMaxId();
+        System.out.println(employee);
+        Assert.assertNotNull(employee);
+
+    }
+
     @Test
     public void findByNameInOrAgeLessThan() throws Exception {
         List<String> list = new ArrayList<String>();
         list.add("test16");
         list.add("LUBAN");
         list.add("DIRENJIE");
-        List<Employee> employeeList = repository.findByNameInOrAgeLessThan(list,20);
+        List<Employee> employeeList = repository.findByNameInOrAgeLessThan(list, 20);
         for (Employee employee : employeeList) {
             System.out.println(employee);
         }
-        Assert.assertTrue(employeeList.size()>0);
+        Assert.assertTrue(employeeList.size() > 0);
     }
 
     @Test
     public void findByNameStartingWithAndAgeLessThan() throws Exception {
-        List<Employee> employeeList = repository.findByNameStartingWithAndAgeLessThan("test",20);
+        List<Employee> employeeList = repository.findByNameStartingWithAndAgeLessThan("test", 20);
         for (Employee employee : employeeList) {
             System.out.println(employee);
         }
-        Assert.assertTrue(employeeList.size()>0);
+        Assert.assertTrue(employeeList.size() > 0);
     }
 
     @Test
@@ -47,7 +101,7 @@ public class EmployeeRepositoryTest {
         for (Employee employee : employeeList) {
             System.out.println(employee);
         }
-        Assert.assertTrue(employeeList.size()>0);
+        Assert.assertTrue(employeeList.size() > 0);
     }
 
 
